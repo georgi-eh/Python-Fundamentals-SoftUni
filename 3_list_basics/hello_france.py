16def1b752fc2a49df2aa0170be66c0b2d8db90d
georgi-eh <georgi.hristov@unwe.bg>
2023-01-17 00:19:39 +0200

    100/100 solution added

--- 3_list_basics/hello_france.py
+++ 3_list_basics/hello_france.py
@@ -1,44 +1,37 @@
-# TODO: ONLY 70/100
+collection_of_items = input().split("|")
+start_budget = float(input())
 
-ticket_price = 150
-items = input().split("|")
-budget = float(input())
-bought_items = []
-new_prices = []
-format_prices = []
-revenue = 0
 profit = 0
+interim_budget = 0
+profit_list = []
 
-CLOTHES_MAX_PRICE = 50
-SHOES_MAX_PRICE = 35
-ACCESSORIES_MAX_PRICE = 20.50
+type_items = [index.split("->")[0] for index in collection_of_items]
+price_items = [float(index.split("->")[1]) for index in collection_of_items]
 
-for item in items:
-    item_type = item.split("->")[0]
-    item_price = float(item.split("->")[1])
+for i in range(len(type_items)):
+    sell_price = 0
+    if type_items[i] == "Clothes" and price_items[i] > 50:
+        continue
+    elif type_items[i] == "Shoes" and price_items[i] > 35:
+        continue
+    elif type_items[i] == "Accessories" and price_items[i] > 20.5:
+        continue
 
-    check_passes = ((item_type == "Clothes" and item_price < CLOTHES_MAX_PRICE and item_price <= budget) or
-                    (item_type == "Shoes" and item_price < SHOES_MAX_PRICE and item_price <= budget) or
-                    (item_type == "Accessories" and item_price < ACCESSORIES_MAX_PRICE and item_price <= budget))
+    if start_budget >= price_items[i]:
+        sell_price += price_items[i] * 1.4
+        profit += price_items[i] * 1.4 - price_items[i]
+        start_budget -= price_items[i]
+        interim_budget += price_items[i] * 1.4
+        profit_list.append(f"{sell_price:.2f}")
 
-    if check_passes:
-        budget -= item_price
-        new_price =  1.4 * item_price
-        revenue += new_price
-        new_prices.append(new_price)
-        bought_items.append(item_type)
-        profit += 0.4 * item_price
+profit_list = " ".join(str(i) for i in profit_list)
 
-for price in new_prices:
-    format_price = format(price, ".2f")
-    format_prices.append(format_price)
-
-
-print(*format_prices)
+print(profit_list)
 print(f"Profit: {profit:.2f}")
 
-if budget + revenue >= ticket_price:
+total_budget = interim_budget + start_budget
+
+if total_budget >= 150:
     print("Hello, France!")
 else:
     print("Not enough money.")
-
